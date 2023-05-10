6173762af1e8b3670037db80a3bccd788f86eec8
Gabriel Severo <gsevero16@gmail.com>
2023-05-10 17:25:46 -0300

    Ajustes e classe teste do desafio 1091

--- src/main/BeeCrowd1091.java
+++ src/main/BeeCrowd1091.java
@@ -2,27 +2,29 @@ package main;
 
 import java.util.*;
 
-public class beeCrowd1091 {
-    public void calculaPosicao(int k, int n, int m, int x, int y){
+public class BeeCrowd1091 {
+    public string calculaPosicao(int k, int n, int m, int x, int y){
         int constant = k;
         while(constant > 0){
             int first = n, second = m, third = x, forth = y;
+            string coord = "";
 
             for(int i= 0; i < constant; i++){
                 int dx = third - first, dy = forth - second;
 
                 if(dx == 0 || dy == 0){
-                    System.out.println("divisa");
+                    coord = "divisa";
                 } else if (dx > 0 && dy > 0){
-                    System.out.println("NE");
+                    coord = "NE";
                 }else if (dx > 0){
-                    System.out.println("SE");
+                    coord = "SE";
                 }else if (dy > 0){
-                    System.out.println("NO");
+                    coord = "NO";
                 }else{
-                    System.out.println("SO");
+                    coord = "SO";
                 }
             }
+            return coord;
         }
     }
 }
---
+++ target/test-classes/BeeCrowd1091Test.java
@@ -0,0 +1,40 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class BeeCrowd1091Test {
+    
+    @Test
+    public void coordNETest(){
+        BeeCrowd1091 bc = new BeeCrowd1091();
+        String result = bc.calculaPosicao(1,2,2,2);
+        assertEquals("NE", result);
+    }
+
+    @Test
+    public void coordSETest(){
+        BeeCrowd1091 bc = new BeeCrowd1091();
+        String result = bc.calculaPosicao(1,2,2,2);
+        assertEquals("SE", result);
+    }
+
+    @Test
+    public void coordNOTest(){
+        BeeCrowd1091 bc = new BeeCrowd1091();
+        String result = bc.calculaPosicao(1,2,2,2);
+        assertEquals("NO", result);
+    }
+
+    @Test
+    public void coordSOTest(){
+        BeeCrowd1091 bc = new BeeCrowd1091();
+        String result = bc.calculaPosicao(1,2,2,2);
+        assertEquals("SO", result);
+    }
+
+    @Test
+    public void coordDividaTest(){
+        BeeCrowd1091 bc = new BeeCrowd1091();
+        String result = bc.calculaPosicao(1,0,0,0);
+        assertEquals("Divisa", result);
+    }
+}
