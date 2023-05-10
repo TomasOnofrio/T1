06e6964e1f116ccab9a296c082a6b544035014e1
Gabriel Severo <gsevero16@gmail.com>
2023-05-10 18:13:19 -0300

    Feito alguns ajustes e configurado arquivo POM

--- src/main/BeeCrowd1091.java
+++ src/main/BeeCrowd1091.java
@@ -1,4 +1,4 @@
-package main;
+package trabalho;
 
 import java.util.*;
 
--- target/test-classes/BeeCrowd1397Test.java
+++ target/test-classes/BeeCrowd1397Test.java
@@ -1,3 +1,34 @@
+package trabalho;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BeeCrowd1397Test {
-    
+
+    @Test
+    public void testarObterVencedor() {
+        BeeCrowd1397 bc = new BeeCrowd1397();
+
+        int[] jogadasJogador1 = { 1, 2, 3 };
+        int[] jogadasJogador2 = { 2, 3, 1 };
+
+        int resultadoEsperado = 1;
+        int resultadoAtual = bc.obterVencedor(jogadasJogador1, jogadasJogador2);
+
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+
+    @Test
+    public void testarEmpate() {
+        BeeCrowd1397 bc = new BeeCrowd1397();
+
+        int[] jogadasJogador1 = { 1, 2, 3 };
+        int[] jogadasJogador2 = { 3, 2, 1 };
+
+        int resultadoEsperado = 0;
+        int resultadoAtual = bc.obterVencedor(jogadasJogador1, jogadasJogador2);
+
+        assertEquals(resultadoEsperado, resultadoAtual);
+    }
+}
 }
